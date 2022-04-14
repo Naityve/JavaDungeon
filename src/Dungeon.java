4bd30b768d02e11d555cc4dad4762a84e0d9533f
Naityve <longinflorek@gmail.com>
2022-04-15 00:49:48 +0100

    first display setup

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -6,4 +6,43 @@ public class Dungeon {
         System.out.println("Hello World!");    
     }
 
+    public static void Display() {
+
+        String[] height = new String[8];
+        String[] width = new String[32];
+
+        String[][] map = new String[8][32];
+
+        map[0][0] = "/";
+
+        String symbol_buffer = "/";
+
+        // Initialise the display with emptySpace symbols : '/'
+
+        for(int i=0; i<height.length; i++) {
+            for(int j=0; j<width.length; j++) {
+
+                width[j] = symbol_buffer;
+
+            }
+
+        }
+
+        // Prints out the display
+
+        for(int i=0; i<height.length; i++) {
+
+            System.out.println();
+
+            for(int j=0; j<width.length; j++) {
+
+                System.out.print(width[j]);
+
+            }
+        }
+    }
+
+    public static void clearDisplay() {
+        System.out.println("\033[H\033[2J");
+    }
 }
